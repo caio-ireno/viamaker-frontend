4c9553eee60e1269e1124a0531ffcabbca4fb62b
caio-ireno <caio_ireno@hotmail.com>
2023-07-15 15:38:39 -0300

    Feat: page and req api

--- src/pages/Produtos.tsx
+++ src/pages/Produtos.tsx
@@ -1,7 +1,29 @@
+import { useEffect, useState } from 'react'
+import { ProdutoProps, ProdutoServices } from '../shared/services/produtoApi'
+import { Typography } from 'antd'
+
 export const Produtos = () => {
+  const [rows, setRows] = useState<ProdutoProps[]>([])
+
+  useEffect(() => {
+    ProdutoServices.getAll().then(result => {
+      if (result instanceof Error) {
+        alert(result.message)
+        return
+      } else {
+        setRows(result)
+      }
+    })
+  }, [])
+
   return (
     <div>
-      <div>Produtos</div>
+      {rows.map(row => (
+        <div key={row._id}>
+          <Typography style={{ color: '#000' }}>{row.nome}</Typography>
+          <Typography>{row.description}</Typography>
+        </div>
+      ))}
     </div>
   )
 }
