b2dfa341aa4e006d237c7dc45d1fad85f704d3a5
caio-ireno <caio_ireno@hotmail.com>
2023-07-15 15:42:57 -0300

    Feat: user page and api

--- src/pages/Usuarios.tsx
+++ src/pages/Usuarios.tsx
@@ -1,7 +1,28 @@
+import { useEffect, useState } from 'react'
+import { Typography } from 'antd'
+import { UserServices, UsuarioProps } from '../shared/services/userApi'
+
 export const Usuarios = () => {
+  const [rows, setRows] = useState<UsuarioProps[]>([])
+
+  useEffect(() => {
+    UserServices.getAll().then(result => {
+      if (result instanceof Error) {
+        alert(result.message)
+        return
+      } else {
+        setRows(result)
+      }
+    })
+  }, [])
+
   return (
     <div>
-      <div>Usuarios</div>
+      {rows.map(row => (
+        <div key={row._id}>
+          <Typography style={{ color: '#000' }}>{row.nome}</Typography>
+        </div>
+      ))}
     </div>
   )
 }
---
+++ src/shared/services/userApi.ts
@@ -0,0 +1,25 @@
+import { Api } from "./axios"
+
+export interface UsuarioProps {
+  _id: number;
+  nome: string;
+}
+
+const getAll = async (): Promise<UsuarioProps[]> => {
+  try {
+    const urlRelativa = '/usuario'
+    const { data } = await Api.get(urlRelativa)
+    console.log(data)
+    return data
+
+  } catch (error) {
+    console.error(error)
+    throw new Error(
+      (error as { message: string }).message || 'Erro ao Carregar',
+    );
+  }
+}
+
+export const UserServices = {
+  getAll,
+}
