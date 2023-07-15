eb7cf6b14ab6fb4f47188d6901e52a38b5e71171
caio-ireno <caio_ireno@hotmail.com>
2023-07-15 18:17:18 -0300

    Feat: user style

--- src/pages/Usuarios.tsx
+++ src/pages/Usuarios.tsx
@@ -1,8 +1,12 @@
-import { useEffect, useState } from 'react'
-import { Typography } from 'antd'
+import { useContext, useEffect, useState } from 'react'
+import { Typography, Row, Col } from 'antd'
 import { UserServices, UsuarioProps } from '../shared/services/userApi'
+import { AntThemeContext } from '../shared/context/AntThemeContext'
 
 export const Usuarios = () => {
+  const theme = useContext(AntThemeContext)
+  const { Title } = Typography
+
   const [rows, setRows] = useState<UsuarioProps[]>([])
 
   useEffect(() => {
@@ -18,11 +22,30 @@ export const Usuarios = () => {
 
   return (
     <div>
-      {rows.map(row => (
-        <div key={row._id}>
-          <Typography style={{ color: '#000' }}>{row.nome}</Typography>
-        </div>
-      ))}
+      <Title level={2}>Usuarios</Title>
+      <Row
+        style={{ padding: 0, display: 'flex', gap: 5, width: '100%' }}
+        justify="space-evenly"
+      >
+        {rows.map(row => (
+          <Col
+            style={{
+              padding: 0,
+              backgroundColor: theme.primary,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}
+            span={24}
+            key={row._id}
+          >
+            <Title style={{ textAlign: 'center' }} level={3}>
+              {row.nome}
+            </Title>
+          </Col>
+        ))}
+      </Row>
     </div>
   )
 }
